2b22cf3a270822e473c7bb648e99e77b096a4070
Nayna Agarwal <nayna9496@gmail.com>
2018-04-21 14:14:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+ /*jslint devel: true */
+/*global
+$*/
+ //TO DO LIST
+ 
+$(function () {
+    "use strict";
+	
+    $('#todoList ul').sortable({
+        items: "li:not('.listTitle, .addItem')",
+        connectWith: "ul",
+        dropOnEmpty: true,
+        placeholder: "emptySpace"
+    });
+    
+    $('input').keydown(function (e) {
+        
+        if (e.keyCode === 13) {
+            var item = $(this).val();
+        
+            $(this).parent().parent().append('<li>' + item + '<li>');
+            $(this).val('');
+         
+        }
+                       
+    });
+
+    $('#trash').droppable({
+        drop: function (event, ui) {
+            ui.draggable.remove();
+        }
+    });
+});
+
+
